7a53d0dca2908a9dce913f424b7714154dc10902
xfry <fry.andrade@gmail.com>
2018-07-23 18:45:33 -0500

    adding new style for social button

--- client/components/button/social.js
+++ client/components/button/social.js
@@ -1,19 +1,31 @@
 import React from "React";
+import {View, StyleSheet} from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 import Style from "./style";
 
 let SocialButton = (props) => {
   return (
-    <Icon.Button 
-        raised
-        primary
-        name="facebook" 
-        backgroundColor="#3b5998"
-        style={Style.social}
-        onPress={props.onPress}>
-        Entrar con Facebook
-    </Icon.Button>
+    <View id="btnContainer" style={styles.buttonContainer}>
+      <Icon.Button 
+          raised
+          primary
+          name="facebook" 
+          backgroundColor="#3b5998"
+          style={Style.social}
+          onPress={props.onPress}>
+          Entrar con Facebook
+      </Icon.Button>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    margin: 20,
+    flexDirection: 'column',
+    justifyContent: "center",
+  }
+});
+
 
 export default SocialButton;
